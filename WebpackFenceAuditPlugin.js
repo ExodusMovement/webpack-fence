959c2a2a9fb000c357f822512fc6fefd65467b38
Nikita Skovoroda <chalkerx@gmail.com>
2023-01-10 08:54:36 +0400

    fixes

--- WebpackFenceAuditPlugin.js
+++ WebpackFenceAuditPlugin.js
@@ -10,6 +10,7 @@ class WebpackFenceAuditPlugin extends WebpackFencePlugin {
   #wrapPath = null
   #seen = new Set()
   #dirs = new Set()
+
   constructor({ rootPath, outPath, wrapPath, ...rest }) {
     assert(Object.keys(rest).length === 0 && rootPath && outPath)
     assert(path.resolve(rootPath) === rootPath)
@@ -19,28 +20,32 @@ class WebpackFenceAuditPlugin extends WebpackFencePlugin {
     this.#outPath = path.resolve(rootPath, outPath)
     this.#wrapPath = wrapPath
   }
+
   addFile(filePath) {
+    if (this.#seen.has(filePath)) return true
+
     // TODO: do we want to also include `modulePackage`?
     const prefix = `${this.#rootPath}${path.sep}`
     assert(path.resolve(filePath) === filePath)
     assert(filePath.startsWith(prefix))
-    if (!this.#seen.has(filePath)) {
-      const file = filePath.slice(prefix.length)
-      let out = path.resolve(this.#outPath, file)
-      if (this.#wrapPath) out = this.#wrapPath(out)
-      if (out) {
-        assert(path.resolve(out) === out)
-        assert(out.startsWith(`${this.#outPath}${path.sep}`))
-        // console.log(`[fencing audit]: Copying ${file} to ${out}`)
-        const dir = path.dirname(filePath)
-        if (!this.#dirs.has(dir)) {
-          fs.mkdirSync(dir, { recursive: true })
-          this.#dirs.add(dir)
-        }
-        fs.cpSync(filePath, out)
+    
+    const file = filePath.slice(prefix.length)
+    let out = path.resolve(this.#outPath, file)
+    if (this.#wrapPath) out = this.#wrapPath(out)
+
+    if (out) {
+      assert(path.resolve(out) === out)
+      assert(out.startsWith(`${this.#outPath}${path.sep}`))
+      // console.log(`[fencing audit]: Copying ${file} to ${out}`)
+      const dir = path.dirname(filePath)
+      if (!this.#dirs.has(dir)) {
+        fs.mkdirSync(dir, { recursive: true })
+        this.#dirs.add(dir)
       }
-      this.#seen.add(filePath)
+      fs.cpSync(filePath, out)
     }
+
+    this.#seen.add(filePath)
     return true
   }
 }
